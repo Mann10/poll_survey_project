0700d3f788f95b77ae883eb40688911622502193
Mann10 <limbachiyamann27@gmail.com>
2024-10-19 11:56:44 +0530

    Initial commit

--- poll_survey_project/settings.py
+++ poll_survey_project/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dj_database_url
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a1&ywwklb&z!kz%-w^%46je4#ak*0s(4u0+b&0!b9_cbxqk)=n'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", False).lower()=='true'
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -84,6 +86,8 @@ DATABASES = {
     }
 }
 
+database_url=os.environ.get("DATABASES_URL")
+DATABASES['default'] =dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
